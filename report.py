9823e5291c397630fb286f32eeba69857d75beff
Gregory <coffeecup@gmail.com>
2019-01-16 22:32:03 +0900

    added tabs

--- report.py
+++ report.py
@@ -6,7 +6,15 @@ import numpy as np
 
 win = tk.Tk()
 win.title("Seller Marketing Report Generator")
+win.geometry('500x500')
 win.resizable(False, False)
+
+row=0
+
+while row < 50:
+    win.rowconfigure(row, weight=1)
+    win.columnconfigure(row, weight=1)
+    row+=1
 
 #Column assignments
 index = 0 #Reserved for index
@@ -26,49 +34,64 @@ netBPV = header + 9
 QCash = header + 10
 netQCashSpending = header + 11
 
+################
+### Notebook ###
+################
+
+nb = ttk.Notebook(win) #Instantiate Notebook widget and place it in main window
+nb.grid(row=1, column=0, rowspan=49, columnspan=50, sticky='NSEW')
+
+#Input tab
+input = ttk.Frame(nb) #Instantiate Frame (tab) widget and attach it to Notebook widget
+nb.add(input, text="Input")
+
+#Report tab
+report = ttk.Frame(nb)
+nb.add(report, text="Report")
+
 #################
 ### Input tab ###
 #################
 
 #Labels
-dateLbl = ttk.Label(win, text="Date").grid(column=label, row=date)
-primeLbl = ttk.Label(win, text="Weekly Prime Seller").grid(column=label, row=prime)
-soldSellerLbl = ttk.Label(win, text="Sold Sellers").grid(column=label, row=soldSeller)
-soldItemsLbl = ttk.Label(win, text="Sold Items: ").grid(column=label, row=soldItems)
-netCartLbl = ttk.Label(win, text="Net Cart").grid(column=label, row=netCart)
-ttlQtyLbl = ttk.Label(win, text="Total Qty").grid(column=label, row=ttlQty)
-netQtyLbl = ttk.Label(win, text="Net Qty").grid(column=label, row=netQty)
-netBPVLbl = ttk.Label(win, text="Net BPV").grid(column=label, row=netBPV)
-QCashLbl = ttk.Label(win, text="No. QCash Sellers").grid(column=label, row=QCash)
-netQCashSpendingLbl = ttk.Label(win, text="Net QCash Spending").grid(column=label, row=netQCashSpending)
+dateLbl = ttk.Label(input, text="Date").grid(column=label, row=date)
+primeLbl = ttk.Label(input, text="Weekly Prime Seller").grid(column=label, row=prime)
+soldSellerLbl = ttk.Label(input, text="Sold Sellers").grid(column=label, row=soldSeller)
+soldItemsLbl = ttk.Label(input, text="Sold Items: ").grid(column=label, row=soldItems)
+netCartLbl = ttk.Label(input, text="Net Cart").grid(column=label, row=netCart)
+ttlQtyLbl = ttk.Label(input, text="Total Qty").grid(column=label, row=ttlQty)
+netQtyLbl = ttk.Label(input, text="Net Qty").grid(column=label, row=netQty)
+netBPVLbl = ttk.Label(input, text="Net BPV").grid(column=label, row=netBPV)
+QCashLbl = ttk.Label(input, text="No. QCash Sellers").grid(column=label, row=QCash)
+netQCashSpendingLbl = ttk.Label(input, text="Net QCash Spending").grid(column=label, row=netQCashSpending)
 
 #Entry boxes
 primeVar = tk.StringVar()
-primeEntry = tk.Entry(win, width=20, textvariable=primeVar).grid(column=entry, row=prime)
+primeEntry = tk.Entry(input, width=20, textvariable=primeVar).grid(column=entry, row=prime)
 
 soldSellerVar = tk.StringVar()
-soldSellerEntry = tk.Entry(win, width=20, textvariable=soldSellerVar).grid(column=entry, row=soldSeller)
+soldSellerEntry = tk.Entry(input, width=20, textvariable=soldSellerVar).grid(column=entry, row=soldSeller)
 
 soldItemsVar = tk.StringVar()
-soldItemsEntry = tk.Entry(win, width=20, textvariable=soldItemsVar).grid(column=entry, row=soldItems)
+soldItemsEntry = tk.Entry(input, width=20, textvariable=soldItemsVar).grid(column=entry, row=soldItems)
 
 netCartVar = tk.StringVar()
-netCartEntry = tk.Entry(win, width=20, textvariable=netCartVar).grid(column=entry, row=netCart)
+netCartEntry = tk.Entry(input, width=20, textvariable=netCartVar).grid(column=entry, row=netCart)
 
 ttlQtyVar = tk.StringVar()
-ttlQtyEntry = tk.Entry(win, width=20, textvariable=ttlQtyVar).grid(column=entry, row=ttlQty)
+ttlQtyEntry = tk.Entry(input, width=20, textvariable=ttlQtyVar).grid(column=entry, row=ttlQty)
 
 netQtyVar = tk.StringVar()
-netQtyEntry = tk.Entry(win, width=20, textvariable=netQtyVar).grid(column=entry, row=netQty)
+netQtyEntry = tk.Entry(input, width=20, textvariable=netQtyVar).grid(column=entry, row=netQty)
 
 netBPVVar = tk.StringVar()
-netBPVEntry = tk.Entry(win, width=20, textvariable=netBPVVar).grid(column=entry, row=netBPV)
+netBPVEntry = tk.Entry(input, width=20, textvariable=netBPVVar).grid(column=entry, row=netBPV)
 
 QCashVar = tk.StringVar()
-QCashEntry = tk.Entry(win, width=20, textvariable=QCashVar).grid(column=entry, row=QCash)
+QCashEntry = tk.Entry(input, width=20, textvariable=QCashVar).grid(column=entry, row=QCash)
 
 netQCashSpendingVar = tk.StringVar()
-netQCashSpendingEntry = tk.Entry(win, width=20, textvariable=netQCashSpendingVar).grid(column=entry, row=netQCashSpending)
+netQCashSpendingEntry = tk.Entry(input, width=20, textvariable=netQCashSpendingVar).grid(column=entry, row=netQCashSpending)
 #Buttons
 
 #Date Picker
